ac04514371ad72695b66166a9c3d0da2fbd0be9b
Gonzalo <bustamantegonzalo208@gmail.com>
2024-12-07 01:59:29 -0300

    Update credenciales.js

--- credenciales.js
+++ credenciales.js
@@ -4,12 +4,12 @@ import { getAuth, signOut } from "firebase/auth"; // Asegúrate de importar sign
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD7olXr4V-2X6e3eyLIGGFFdvP4xLa40f8",
-  authDomain: "app1-5.firebaseapp.com",
-  projectId: "app1-5",
-  storageBucket: "app1-5.firebasestorage.app",
-  messagingSenderId: "337578760044",
-  appId: "1:337578760044:web:54a4e2f172238d46ce4549",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 // Initialize Firebase
